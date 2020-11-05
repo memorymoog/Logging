6207e0ca9d9e72528684327334c44eafb0db4168
David Campbell III <lapidistcubed@gmail.com>
2020-11-04 21:28:02 -0600

    testing

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print ("hello world")
-print("it's working!")
+import requests
+
+#https://[host]/CuratorGateway/connect/authorize?client_id=0a677a41c5f7491d949c9eb4b1098ab0&redirect_uri=https://myapp.com&response_type=code&grant_type=authorization_code&scope=Curator.Server.API Curator.Gateway.API
+url = "https://localhost:54279/CuratorGateway/api/v1"
+
+payload = {}
+headers = {
+  'Authorization': 'Bearer [access token]'
+}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
